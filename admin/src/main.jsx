3cf4ee57c56cd7d937c5dd5935d615f0d6ec0b92
Shafeeque27 <muhammedshafeequek27@gmail.com>
2025-11-14 10:11:29 +0530

    integrated api"s in frontend admin

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,9 +2,18 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { AppContext } from './context/AppContext.jsx';
+import AdminContextProvider from './context/AdminContext.jsx';
+import DoctorContextProvider from './context/DoctorContext.jsx';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <App />
+        <AdminContextProvider>
+            <DoctorContextProvider>
+                <AppContext>
+                    <App />
+                </AppContext>
+            </DoctorContextProvider>
+        </AdminContextProvider>
     </BrowserRouter>
 );
---
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect } from 'react';
+import { AdminContext } from '../../context/AdminContext';
+
+const DoctorsList = () => {
+    const { doctors, aToken, getAllDoctors, changeAvailability } = useContext(AdminContext);
+
+    useEffect(() => {
+        if (aToken) {
+            getAllDoctors();
+        }
+    }, [aToken]);
+    return (
+        <div className="m-5 max-h-[90vh] overflow-y-scroll">
+            <h1 className="text-lg font-medium">All Doctors</h1>
+            <div className="w-full flex flex-wrap gap-4 pt-5 gap-y-6">
+                {doctors.map((doc, idx) => (
+                    <div
+                        className="border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer group"
+                        key={idx}>
+                        <img
+                            className="bg-indigo-50 group-hover:bg-primary transition-all duration-500"
+                            src={doc.image}
+                            alt=""
+                        />
+                        <div className="p-4">
+                            <p className="text-neutral-800 text-lg font-medium">{doc.name}</p>
+                            <p className="text-zinc-600 text-sm">{doc.speciality}</p>
+                            <div className="mt-2 flex items-center gap-1 text-sm">
+                                <input
+                                    onChange={() => changeAvailability(doc._id)}
+                                    type="checkbox"
+                                    checked={doc.available}
+                                />
+                                <p>Available</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default DoctorsList;
